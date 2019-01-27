22e977cf91e2e9a84b119529446c182693fd20f9
luigi-zuniga <luis.alberto.zuniga@outlook.com>
2019-01-26 22:09:42 -0300

    ANGULAR7 CRUD - Router and Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+
+import { Route, RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+
+const routes: Route[] = [
+  {path:'' , component: HomeComponent},
+  {path:'home' , component: HomeComponent},
+  {path:'form' , component: FormComponent},
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
